d705b97571ef58e1359d4d6bb77d2a4be4876261
Sam Trapskin <samtrapskin@gmail.com>
2018-06-12 16:20:48 -0500

    React course

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import BooksReducer from './book_reducer';
+
+
+const rootReducer = combineReducers({
+  books:BooksReducer
+});
+
+export default rootReducer;
